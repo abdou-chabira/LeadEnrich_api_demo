10ec3a87c47fbca6dad8acc83148bd3f031adc62
achabira <abderrahmane@yobi.app>
2026-02-27 15:44:10 +0100

    Refactor DATABASES configuration to remove default values for environment variables

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # or mysql, sqlite3, etc.
-        'NAME': os.environ.get("DB_NAME", "your_db_name"),
-        'USER': os.environ.get("DB_USER", "your_db_user"),
-        'PASSWORD': os.environ.get("DB_PASSWORD", "your_password"),
-        'HOST': os.environ.get("DB_HOST", "localhost"),  # or IP/hostname
-        'PORT': os.environ.get("DB_PORT", "5432"),       # optional, depends on DB
-        'DATABASE_URL': os.environ.get("DATABASE_URL", "postgres://user:password@localhost:5432/your_db_name"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
+        'DATABASE_URL': os.environ.get("DATABASE_URL"),
     }
 }
 TEMPLATES = [
